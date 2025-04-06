95b7c7f07f99058a2d173dc54ce4394945ac932e
Ruibin Zhang <ruibin.zhang@advancegroup.com>
2025-04-06 21:44:04 +0800

    enable highligt and responsice ui

--- back_trace.py
+++ back_trace.py
@@ -1,15 +1,21 @@
 import sys
+from PyQt5.QtWidgets import (
+    QApplication, QWidget, QLabel, QLineEdit, QTextEdit, QPushButton,
+    QGridLayout, QVBoxLayout, QScrollArea, QSizePolicy, QMessageBox
+)
+
 import mysql.connector
 import json
 import os
 import getpass
 import socket
 from datetime import datetime, timedelta
-from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QFormLayout, QLabel, QLineEdit, QPushButton, QTextEdit, QScrollArea, QMessageBox
 import sqlparse
 from pygments import highlight
 from pygments.formatters import HtmlFormatter
 from pygments.lexers import SqlLexer
+
+
 
 CONFIG_FILE = "config.json"
 
@@ -27,7 +33,6 @@ def save_last_event_time(event_time):
     with open(CONFIG_FILE, "w") as f:
         json.dump(config, f)
 
-
 def get_default_config():
     if os.path.exists(CONFIG_FILE):
         with open(CONFIG_FILE, "r") as f:
@@ -43,7 +48,6 @@ def get_default_config():
             "event_time": (datetime.now() - timedelta(minutes=10)).strftime("%Y-%m-%d %H:%M:%S")
         }
     return default_config
-    
 
 def get_default_event_time():
     return (datetime.now() - timedelta(minutes=10)).strftime("%Y-%m-%d %H:%M:%S")
@@ -54,142 +58,210 @@ def format_and_highlight_sql(query: str) -> str:
     
     # Highlight SQL
     highlighted_sql = highlight(formatted_sql, SqlLexer(), HtmlFormatter())
+    print(highlighted_sql)
     
     return highlighted_sql
 
-def run_query(default_config):
-    username = username_input.text().strip() or default_config["username"]
-    password = password_input.text().strip() or default_config["password"]
-    host = host_input.text().strip() or default_config["host"]
-    port = port_input.text().strip() or default_config["port"]
-    query_user = query_username_input.text().strip() or default_config["query_username"]
-    query_host = query_host_input.text().strip() or default_config["query_host"]
-    user_host = f"{query_user}[{query_user}] @  [{query_host}]"
-    event_time = event_time_input.text().strip()
 
-    #  save the config to the file
-    config = {
-        "username": username,
-        "password": password,
-        "host": host,
-        "port": port,
-        "query_username": query_user,
-        "query_host": query_host,
-        "event_time": event_time
-    }
-    with open(CONFIG_FILE, "w") as f:
-        json.dump(config, f)
-
-
-    if not event_time:
-        event_time = load_last_event_time() or get_default_event_time()
+def get_pygments_styles():
+    return HtmlFormatter().get_style_defs('.highlight')
     
 
-    try:
-        conn = mysql.connector.connect(
-            host=host,
-            port=port,
-            user=username,
-            password=password,
-            database="mysql"
-        )
 
-        cursor = conn.cursor()
-        query = (
-            "SELECT event_time, argument AS sql_text FROM mysql.general_log "
-            "WHERE event_time > %s "
-            "AND user_host = %s "
-            "ORDER BY event_time"
-        )
+class ResponsiveForm(QWidget):
+    def __init__(self, default_config, output_text):
+        super().__init__()
+        self.default_config = default_config
+        self.output_text = output_text
+        self.inputs = []
+        self.fields = []
+        self.keys_map = {
+            "username": 0,
+            "password": 1,
+            "host": 2,
+            "port": 3,
+            "query_username": 4,
+            "query_host": 5,    
+            "event_time": 6
+        }
+        self.init_ui()
+        
 
-        cursor.execute(query, (event_time, user_host))
-        results = cursor.fetchall()
-        output_text.clear()
+    def init_ui(self):
+        self.setWindowTitle("Responsive MySQL Log Viewer")
+        self.resize(800, 600)
 
-        last_time = None
-        for row in results:
-            event_time = row[0]
-            sql_text = row[1] # it is a byte now convert it to string
-            if isinstance(sql_text, bytes):
-                sql_text = sql_text.decode('utf-8')
-            if sql_text == "" or sql_text is None:
-                sql_text = "Empty SQL statement"
-            if len(sql_text) > 1000:
-                sql_text = sql_text[:1000] + "..."
-            formatted_highlighted_sql = format_and_highlight_sql(sql_text)
-            print(formatted_highlighted_sql)
-            output_text.append(f"{event_time}{formatted_highlighted_sql}<br/>")
-            last_time = row[0]
+        # Main layout
+        self.main_layout = QVBoxLayout(self)
+        self.main_layout.setContentsMargins(10, 10, 10, 10)
+        self.main_layout.setSpacing(8)
 
-        if last_time:
-            save_last_event_time(last_time.strftime("%Y-%m-%d %H:%M:%S"))
+        # Scrollable input form
+        self.scroll_area = QScrollArea()
+        self.scroll_area.setWidgetResizable(True)
+        self.scroll_area.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Preferred)
+        self.scroll_area.setMaximumHeight(300)  # Limit height to keep output visible
 
-        if not results:
-            output_text.append("No results found.\n")
+        self.form_widget = QWidget()
+        self.grid_layout = QGridLayout(self.form_widget)
+        self.grid_layout.setSpacing(4)
 
-        cursor.close()
-        conn.close()
+        self.scroll_area.setWidget(self.form_widget)
+        self.main_layout.addWidget(self.scroll_area)
 
-    except Exception as e:
-        QMessageBox.critical(window, "Error", str(e))
+        # Define fields
+        self.fields = [
+            ("MySQL Username:", QLineEdit()),
+            ("MySQL Password:", QLineEdit()),
+            ("Host IP:", QLineEdit()),
+            ("Port:", QLineEdit()),
+            ("Query Username:", QLineEdit()),
+            ("Query Host:", QLineEdit()),
+            ("Event Time:", QLineEdit())
+        ]
+
+        keys = self.keys_map.keys()
+        
+        for (label, input_field), key in zip(self.fields,keys):
+            input_field.setText(self.default_config[key])
+            input_field.setMinimumHeight(25)
+            if "password" in label.lower():
+                input_field.setEchoMode(QLineEdit.Password)
+            self.inputs.append((label, input_field))
+
+        self.update_layout()
+        self.installEventFilter(self)
+
+        # Run Button
+        run_button = QPushButton("Run Query")
+        run_button.setFixedHeight(30)
+        run_button.clicked.connect(self.on_run_query)
+        self.main_layout.addWidget(run_button)
+
+        # Output Area
+        self.output_text.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
+
+        self.main_layout.addWidget(self.output_text, stretch=1)
+
+    def on_run_query(self):
+        config = get_default_config()
+        self.output_text.setText(f"Running query with config:\n{config}")
+        username = self.inputs[self.keys_map["username"]][1].text().strip() or default_config["username"]
+        password = self.inputs[self.keys_map["password"]][1].text().strip() or default_config["password"]
+        host = self.inputs[self.keys_map["host"]][1].text().strip() or default_config["host"]
+        port = self.inputs[self.keys_map["port"]][1].text().strip() or default_config["port"]
+        query_user = self.inputs[self.keys_map["query_username"]][1].text().strip() or default_config["query_username"]
+        query_host = self.inputs[self.keys_map["query_host"]][1].text().strip() or default_config["query_host"]
+        user_host = f"{query_user}[{query_user}] @  [{query_host}]"
+        event_time = self.inputs[self.keys_map["event_time"]][1].text().strip() or default_config["event_time"]
+
+        #  save the config to the file
+        config = {
+            "username": username,
+            "password": password,
+            "host": host,
+            "port": port,
+            "query_username": query_user,
+            "query_host": query_host,
+            "event_time": event_time
+        }
+        with open(CONFIG_FILE, "w") as f:
+            json.dump(config, f)
 
 
+        if not event_time:
+            event_time = load_last_event_time() or get_default_event_time()
+        
+
+        try:
+            conn = mysql.connector.connect(
+                host=host,
+                port=port,
+                user=username,
+                password=password,
+                database="mysql"
+            )
+
+            cursor = conn.cursor()
+            query = (
+                "SELECT event_time, argument AS sql_text FROM mysql.general_log "
+                "WHERE event_time > %s "
+                "AND user_host = %s "
+                "ORDER BY event_time"
+            )
+
+            cursor.execute(query, (event_time, user_host))
+            results = cursor.fetchall()
+            output_text.clear()
+
+            css_styles = get_pygments_styles()
+            html_output = f"<style>{css_styles}</style>"
+
+            last_time = None
+            for row in results:
+                event_time = row[0]
+                sql_text = row[1] # it is a byte now convert it to string
+                if isinstance(sql_text, bytes):
+                    sql_text = sql_text.decode('utf-8')
+                if sql_text == "" or sql_text is None:
+                    sql_text = "<p style='color:red;'>Empty SQL statement</p>"
+                else:
+                    if len(sql_text) > 1000:
+                        sql_text = sql_text[:1000] + "..."
+                    formatted_highlighted_sql = format_and_highlight_sql(sql_text)
+                    print(formatted_highlighted_sql)
+                    html_output += f"<b>{event_time}</b>{formatted_highlighted_sql}<br/>"
+                    last_time = row[0]
+
+            if last_time:
+                save_last_event_time(last_time.strftime("%Y-%m-%d %H:%M:%S"))
+                self.inputs[self.keys_map["event_time"]][1].setText(last_time.strftime("%Y-%m-%d %H:%M:%S.%f"))
+
+            if not results:
+                # the css style is red
+                html_output += "<p style='color:red;'>No results found.</p>"
+
+            cursor.close()
+            conn.close()
+            self.output_text.setHtml(html_output)
+        except Exception as e:
+            QMessageBox.critical(window, "Error", str(e))
+        
+
+    def eventFilter(self, source, event):
+        if event.type() == event.Resize:
+            self.update_layout()
+        return super().eventFilter(source, event)
+
+    def update_layout(self):
+        while self.grid_layout.count():
+            item = self.grid_layout.takeAt(0)
+            widget = item.widget()
+            if widget:
+                widget.setParent(None)
+
+        width = self.width()
+        # Calculate the number of columns based on the width
+        columns = max(1, width // 400)
+
+        for index, (label_text, input_field) in enumerate(self.inputs):
+            row = index // columns
+            col = (index % columns) * 2
+            label = QLabel(label_text)
+            label.setMinimumHeight(20)
+            self.grid_layout.addWidget(label, row, col)
+            self.grid_layout.addWidget(input_field, row, col + 1)
 
 
-default_config = get_default_config()
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    default_config = get_default_config()
 
+    output_text = QTextEdit()
+    output_text.setReadOnly(True)
+    output_text.setAcceptRichText(True)
 
-# UI Setup
-app = QApplication(sys.argv)
-window = QWidget()
-window.setWindowTitle("BackTrace - MySQL Query Log Viewer")
+    window = ResponsiveForm(default_config, output_text)
+    window.show()
 
-layout = QVBoxLayout()
-
-form_layout = QFormLayout()
-
-username_input = QLineEdit()
-username_input.setText(default_config["username"])
-form_layout.addRow(QLabel("MySQL Username:"), username_input)
-
-password_input = QLineEdit()
-password_input.setText(default_config["password"])
-password_input.setEchoMode(QLineEdit.Password)
-form_layout.addRow(QLabel("MySQL Password:"), password_input)
-
-host_input = QLineEdit()
-host_input.setText(default_config["host"])
-form_layout.addRow(QLabel("Host IP:"), host_input)
-
-port_input = QLineEdit()
-port_input.setText(default_config["port"])
-form_layout.addRow(QLabel("Port:"), port_input)
-
-query_username_input = QLineEdit()
-query_username_input.setText(default_config["query_username"])
-form_layout.addRow(QLabel("Query Username:"), query_username_input)
-query_host_input = QLineEdit()
-query_host_input.setText(default_config["query_host"])
-form_layout.addRow(QLabel("Query Host:"), query_host_input)
-
-event_time_input = QLineEdit()
-event_time_input.setText(default_config["event_time"])
-form_layout.addRow(QLabel("Event Time:"), event_time_input)
-
-
-
-layout.addLayout(form_layout)
-
-run_button = QPushButton("Run Query")
-run_button.clicked.connect(lambda: run_query(get_default_config()))
-layout.addWidget(run_button)
-
-output_text = QTextEdit()
-output_text.setReadOnly(True)
-output_text.setAcceptRichText(True)
-layout.addWidget(output_text)
-
-window.setLayout(layout)
-window.show()
-
-sys.exit(app.exec_())
+    sys.exit(app.exec_())
